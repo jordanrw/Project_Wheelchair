8cfd1b98535c2332fbffcaf0dc14e4c018e35d2e
White, Jordan <jwhite3@gannett.com>
2014-08-05 09:25:03 -0400

    pod file included

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// TapkuLibrary
+#define COCOAPODS_POD_AVAILABLE_TapkuLibrary
+#define COCOAPODS_VERSION_MAJOR_TapkuLibrary 0
+#define COCOAPODS_VERSION_MINOR_TapkuLibrary 3
+#define COCOAPODS_VERSION_PATCH_TapkuLibrary 3
+
